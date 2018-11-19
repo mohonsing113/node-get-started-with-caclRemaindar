38e5ff6e847f30fbfb159a34739e3d240fc93804
Mo Hon Sing <h.s.mo@oocl.com>
2018-11-19 16:46:20 +0800

    finished tests

--- main/main.js
+++ main/main.js
@@ -1,9 +1,24 @@
 // Write your cade below:
-function caclRemaindar() {};
+function caclRemaindar(v1,v2) { return v1%v2};
 
-function caclSum() {};
+function caclSum(array) {
 
-function caclSumInConditon() {};
+    var sum =0;
+    for(var i = 0 ; i<array.length;i++){
+        sum+=array[i];
+    }
+    return sum;
+};
+
+function caclSumInConditon(array, val) {
+    var sum =0;
+    for(var i = 0 ; i<array.length;i++){
+        if (val>array[i]){
+            sum+=array[i];
+        }
+    }
+    return sum;
+};
 
 module.exports = {
     caclRemaindar,
